8bf40d886c562c18658135a5f0ea3ac11cb49b4b
Techbear <techbearHub@users.noreply.github.com>
2019-11-14 23:37:33 +0200

    All the elements added for visual+manifest.xml so internet is used

--- app/src/main/java/com/data/androiddemo/Fragments/ShareFragment.java
+++ app/src/main/java/com/data/androiddemo/Fragments/ShareFragment.java
@@ -5,21 +5,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 import com.data.androiddemo.R;
 
-public class ShareFragment extends Fragment implements View.OnClickListener {
+public class ShareFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View myView = inflater.inflate(R.layout.share_fragment, container, false);
 
+        WebView dogview = myView.findViewById(R.id.dogView);
+
+        dogview.loadUrl("https://swedbank.se/");
+        float scalingFactor = 0.9f; // scale down to half the size
+        dogview.setScaleX(scalingFactor);
+        dogview.setScaleY(scalingFactor);
 
         return myView;
     }
 
-    @Override
-    public void onClick(View v) {
 
-    }
 }
